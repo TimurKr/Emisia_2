574cd61d0006dacbd4c020801c1f39dd1dc45ffa
Timur Kramár <timurjan.kramar123@gmail.com>
2022-05-08 14:29:03 +0200

    typedef struct TWN and CTWL

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+typedef struct TWN{
+    float data;
+    struct TWN *prev;
+    struct TWN *next;
+}TWN;
+
+typedef struct{
+    TWN *cur;
+}CTWL;
+
+
 int main() {
     printf("Hello, World!\n");
     return 0;
