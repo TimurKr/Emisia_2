506b6bc7d4db403dca64881e805ef4f80e840c55
Timur Kramár <timurjan.kramar123@gmail.com>
2022-05-08 14:59:51 +0200

    ctwl_create_random CTWL and first TWN

--- src/main.c
+++ src/main.c
@@ -12,16 +12,35 @@ typedef struct{
 }CTWL;
 
 CTWL *ctwl_create_empty(void){
+
+    // Create structure for ctwl
     CTWL *ctwl = malloc(sizeof(CTWL));
     if (ctwl == NULL){
         return NULL;
     }
+
     ctwl->cur = NULL;
     return ctwl;
 }
 
 CTWL *ctwl_create_random(unsigned int size){
+    // Create structure for ctwl
+    CTWL *ctwl = malloc(sizeof(CTWL));
+    if (ctwl == NULL){
+        return NULL;
+    }
 
+    // Create first node TWN in ctwl and set cursor to it
+    ctwl->cur = malloc(sizeof(TWN));
+    if (ctwl->cur == NULL){
+        return NULL;
+    }
+
+    for (int i = 0; i < size; i++){
+        ctwl->cur = malloc(sizeof(TWN));
+    }
+
+    return ctwl;
 }
 
 int main() {
