96a639785493205ac5a0150a93e404e23bdf900c
Timur Kramár <timurjan.kramar123@gmail.com>
2022-05-08 14:45:20 +0200

    ctwl_create prototypy

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct TWN{
     float data;
@@ -10,6 +11,13 @@ typedef struct{
     TWN *cur;
 }CTWL;
 
+CTWL *ctwl_create_empty(void){
+
+}
+
+CTWL *ctwl_create_random(unsigned int size){
+
+}
 
 int main() {
     printf("Hello, World!\n");
